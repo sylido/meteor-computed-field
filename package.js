a2a98f7090bee41535133d9750ef456186debcdd
Danail Gabenski <dgabenski@fortressinfosec.com>
2025-06-25 18:36:27 -0600

    Updating for Meteor 3.x
    Updated version to 0.11.0 - changed reference to sylido
    Updated dependencies of the package so it works with Meteor 3.2.2+
    Updated other peerlibrary dependencies

--- package.js
+++ package.js
@@ -1,63 +1,63 @@
 Package.describe({
-  name: 'peerlibrary:computed-field',
-  summary: "Reactively computed field for Meteor",
-  version: '0.10.0',
-  git: 'https://github.com/peerlibrary/meteor-computed-field.git'
+  name    : "sylido:computed-field",
+  summary : "Reactively computed field for Meteor",
+  version : "0.11.0",
+  git     : "https://github.com/sylido/meteor-computed-field.git"
 });
 
 Package.onUse(function (api) {
-  api.versionsFrom('METEOR@1.8.1');
+  api.versionsFrom("METEOR@3.2.2");
 
   // Core dependencies.
   api.use([
-    'ecmascript',
-    'tracker',
-    'reactive-var',
-    'underscore'
+    "ecmascript",
+    "tracker@1.3.4",
+    "reactive-var@1.0.13",
+    "underscore@1.6.4"
   ]);
 
   api.use([
-    'blaze@2.3.3'
+    "blaze@3.0.2"
   ], {weak: true});
 
-  api.export('ComputedField');
+  api.export("ComputedField");
 
-  api.mainModule('lib.js');
+  api.mainModule("lib.js");
 });
 
 Package.onTest(function (api) {
-  api.versionsFrom('METEOR@1.8.1');
+  api.versionsFrom("METEOR@3.2.2");
 
   // Core dependencies.
   api.use([
-    'coffeescript@2.4.1',
-    'ecmascript',
-    'tracker',
-    'reactive-var',
-    'templating',
-    'blaze@2.2.1',
-    'spacebars',
-    'underscore',
-    'jquery'
+    "coffeescript@2.4.1",
+    "ecmascript",
+    "tracker",
+    "reactive-var",
+    "templating",
+    "blaze@2.2.1",
+    "spacebars",
+    "underscore",
+    "jquery"
   ]);
 
   // Internal dependencies.
   api.use([
-    'peerlibrary:computed-field'
+    "sylido:computed-field"
   ]);
 
   // 3rd party dependencies.
   api.use([
-    'peerlibrary:classy-test@0.4.0'
+    "sylido:classy-test@0.4.0"
   ]);
 
   api.addFiles([
-    'tests.coffee'
+    "tests.coffee"
   ]);
 
   api.addFiles([
-    'tests_client.html',
-    'tests_client.coffee',
-    'tests_client.css'
-  ], 'client');
+    "tests_client.html",
+    "tests_client.coffee",
+    "tests_client.css"
+  ], "client");
 });
